7bf219032664a08fcc54cabbb321b5234c91bba5
2sssg <lsg020300@naver.com>
2021-07-30 18:27:34 +0900

    Feat : add MeetController
    add MeetController(Websocket Controller)
    * temporary postit websocket

--- src/main/java/com/HALEEGO/meetin/controller/MeetController.java
+++ src/main/java/com/HALEEGO/meetin/controller/MeetController.java
@@ -1,16 +1,45 @@
 package com.HALEEGO.meetin.controller;
 
 
+import com.HALEEGO.meetin.Constant.Enum.ErrorCode;
+import com.HALEEGO.meetin.DTO.PostItDTO;
+import com.HALEEGO.meetin.DTO.UserDTO;
+import com.HALEEGO.meetin.repository.RoomRepository;
+import com.HALEEGO.meetin.repository.UserRepository;
 import lombok.RequiredArgsConstructor;
+import org.json.simple.JSONObject;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.messaging.handler.annotation.MessageMapping;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.messaging.simp.SimpMessageSendingOperations;
+import java.util.*;
 
 @RequiredArgsConstructor
 @RestController
 public class MeetController {
     @Autowired
     private final SimpMessageSendingOperations messagingTemplate;
+
+    @Autowired
+    RoomRepository roomRepository;
+    @Autowired
+    UserRepository userRepository;
+
+    @MessageMapping("/{roomID}/postit")
+    @Transactional
+    public void temp(PostItDTO postItDTO , UserDTO userDTO , @PathVariable("roomID") int roomID ){
+        JSONObject returnObj = new JSONObject();
+
+        returnObj.put("postit" , postItDTO);
+        returnObj.put("user" , userDTO);
+        returnObj.put("status" , ErrorCode.SUCCESS.getStatus());
+        returnObj.put("message" , ErrorCode.SUCCESS.getMessage());
+
+        messagingTemplate.convertAndSend("/topic/"+roomID+"/postit" , returnObj);
+
+    }
 
 
 }
