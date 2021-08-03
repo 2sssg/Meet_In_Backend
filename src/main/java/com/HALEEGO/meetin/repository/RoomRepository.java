54c6d45781924f87603594f46420eb71fb192113
2sssg <lsg020300@naver.com>
2021-08-03 15:49:08 +0900

    Feat : List<User> return
    add findByRoom(Room room) -> return List<User>
    RoomRepository does not changed

--- src/main/java/com/HALEEGO/meetin/repository/RoomRepository.java
+++ src/main/java/com/HALEEGO/meetin/repository/RoomRepository.java
@@ -2,8 +2,10 @@ package com.HALEEGO.meetin.repository;
 
 import com.HALEEGO.meetin.model.Room;
 import org.springframework.data.jpa.repository.JpaRepository;
+import java.util.*;
 
 public interface RoomRepository extends JpaRepository<Room,Long> {
 
     Room findByRoomID(int roomid);
+
 }
--- src/main/java/com/HALEEGO/meetin/repository/User_has_RoomRepository.java
+++ src/main/java/com/HALEEGO/meetin/repository/User_has_RoomRepository.java
@@ -1,7 +1,11 @@
 package com.HALEEGO.meetin.repository;
 
+import com.HALEEGO.meetin.model.Room;
+import com.HALEEGO.meetin.model.User;
 import com.HALEEGO.meetin.model.User_has_Room;
 import org.springframework.data.jpa.repository.JpaRepository;
-
+import java.util.*;
 public interface User_has_RoomRepository extends JpaRepository<User_has_Room,Long> {
+
+    List<User> findByRoom(Room room);
 }
