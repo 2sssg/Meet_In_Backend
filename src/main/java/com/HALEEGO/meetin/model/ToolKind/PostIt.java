db4e4c6c582c9fd38f54a7364dee166b18c14604
2sssg <lsg020300@naver.com>
2021-08-07 13:28:06 +0900

    Feat : Method DTO converter
    make Entity -> DTO method

--- src/main/java/com/HALEEGO/meetin/model/ToolKind/PostIt.java
+++ src/main/java/com/HALEEGO/meetin/model/ToolKind/PostIt.java
@@ -2,6 +2,8 @@ package com.HALEEGO.meetin.model.ToolKind;
 
 
 import com.HALEEGO.meetin.Constant.Enum.Color;
+import com.HALEEGO.meetin.DTO.PostItDTO;
+import com.HALEEGO.meetin.DTO.UserDTO;
 import com.HALEEGO.meetin.model.User;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
@@ -48,4 +50,25 @@ public class PostIt {
     @ManyToOne(fetch = FetchType.LAZY ,cascade = CascadeType.ALL)
     @JoinColumn(name = "TOOL_ID")
     private Tool tool;
+
+
+
+
+    public PostItDTO toPostItDTO(){
+        return new PostItDTO().builder()
+                .id(this.getId())
+                .width(this.getWidth())
+                .height(this.getHeight())
+                .user(
+                        new UserDTO().builder()
+                                .userNAME(this.getUser().getUserNAME())
+                                .build()
+                )
+                .postitID(this.getPostitID())
+                .locationX(this.getLocationX())
+                .locationY(this.getLocationY())
+                .postitCOLOR(this.getPostitCOLOR())
+                .postitCONTEXT(this.getPostitCONTEXT())
+                .build();
+    }
 }
