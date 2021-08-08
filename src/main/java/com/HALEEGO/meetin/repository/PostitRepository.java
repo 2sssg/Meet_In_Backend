b377b8027ee8b6d2906af106f48bb44c04fa95fb
2sssg <lsg020300@naver.com>
2021-08-08 13:07:40 +0900

    Feat : method (QUERY)

--- src/main/java/com/HALEEGO/meetin/repository/PostitRepository.java
+++ src/main/java/com/HALEEGO/meetin/repository/PostitRepository.java
@@ -1,10 +1,12 @@
 package com.HALEEGO.meetin.repository;
 
+import com.HALEEGO.meetin.model.MeetKind.Sixhat;
 import com.HALEEGO.meetin.model.ToolKind.PostIt;
 import com.HALEEGO.meetin.model.ToolKind.Tool;
 import org.springframework.data.jpa.repository.JpaRepository;
 import java.util.*;
 
 public interface PostitRepository extends JpaRepository<PostIt, Long> {
-    List<PostIt> findByTool(Tool tool);
+    List<PostIt> findByTool_Sixhat_Room_RoomID(int roomID);
+    List<PostIt> findByTool_Sixhat(Sixhat sixhat);
 }
