d3a82f0547e47a9718066bdfe685bff7f3d6d250
2sssg <lsg020300@naver.com>
2021-08-10 03:04:10 +0900

    Refactor : code Refactoring

--- src/main/java/com/HALEEGO/meetin/controller/CreateController.java
+++ src/main/java/com/HALEEGO/meetin/controller/CreateController.java
@@ -38,38 +38,37 @@ import java.util.*;
 public class CreateController {
 
     ObjectMapper objectMapper = new ObjectMapper();
-    @Autowired
-    private final SimpMessageSendingOperations messagingTemplate;
-    @Autowired
-    User_has_RoomRepository user_has_roomRepository;
-    @Autowired
-    UserRepository userRepository;
-    @Autowired
-    RoomRepository roomRepository;
-    @Autowired
-    ToolRepository toolRepository;
-    @Autowired
-    SixhatRepository sixhatRepository;
+    @Autowired private final SimpMessageSendingOperations messagingTemplate;
+    @Autowired User_has_RoomRepository user_has_roomRepository;
+    @Autowired UserRepository userRepository;
+    @Autowired RoomRepository roomRepository;
+    @Autowired ToolRepository toolRepository;
+    @Autowired SixhatRepository sixhatRepository;
 
+
+    //////////////////////////////로그인//////////////////////////////////
     @LogExecution
     @RequestMapping(value = "/create/signup" , method = RequestMethod.POST)
     public Object signup(@RequestBody UserDTO userDTO){ //회원가입
-        log.info("userID : "+userDTO.getUserID());
-        log.info("userPW : "+userDTO.getUserPW());
-        log.info("userNAME : "+userDTO.getUserNAME());
-        User user = User.builder()
+        if(userRepository.existsByUserID(userDTO.getUserID())){ // id 겹치는지 확인
+            //겹치는 아이디 있으면 회원가입 실패 오류던짐
+            throw new AlreadyHasUserID("이미 있는 아이디입니다" , ErrorCode.DUPLICATE);
+        }
+        //없으면 진행
+
+        User user = User.builder()  //유저 받아서
                 .userID(userDTO.getUserID())
                 .userPW(userDTO.getUserPW())
                 .userNAME(userDTO.getUserNAME())
                 .build();
-        try {
-            userRepository.save(user);
-            return new FixedreturnValue<>();
-        }catch (Exception e){
-              throw new AlreadyHasUserID("이미 있는 아이디입니다" , ErrorCode.DUPLICATE);
-        }
+
+        userRepository.save(user);//저장
+
+        return new FixedreturnValue<>();
     }
 
+
+    //////////////////////////////방만들기 ///////////////////////////////////////
     @RequestMapping(value = "/create/createroom" , method = RequestMethod.POST)
     @LogExecution
     public Object createRoom(@RequestBody JSONObject jsonObject) {
@@ -78,18 +77,18 @@ public class CreateController {
         Tool tool;
         User_has_Room user_has_room;
         Date time = new Date();
-        Long id = Long.parseLong(jsonObject.get("id").toString());
-        MeetType meetType = MeetType.valueOf(
-                jsonObject.get("meetType").toString()
-        );
+
+        Long id = Long.parseLong(jsonObject.get("id").toString());        //User의 id
+        MeetType meetType = MeetType.valueOf(jsonObject.get("meetType").toString()); // 어떤 회의인지
+        String title = jsonObject.get("title").toString(); //회의 주제
 
         User user = userRepository
                 .findById(id)
                 .orElseThrow(() ->
-                        new NoRoomIDException("id가 없음 ㅋㅋ 다시줭 ㅎㅎ",ErrorCode.NOT_FOUND)
+                        new NoRoomIDException("id로 User 못찾음 \n id : " + id , ErrorCode.NOT_FOUND)
                 );
 
-        room = Room.builder()
+        room = Room.builder() // 룸 만들기
                 .roomID(
                         Math.abs(
                                 new SimpleDateFormat("yyyy-MM-dd HH:mm:ss")
@@ -97,14 +96,16 @@ public class CreateController {
                                         .hashCode()
                         )
                 )
-                .title(jsonObject.get("title").toString())
+                .title(title)
                 .hostUSER(user)
                 .build();
 
 
         switch (meetType){
+            //SIX_HAT이면
             case SIX_HAT:
                 room.setMeetType(MeetType.SIX_HAT);
+
                 sixhat = Sixhat.builder()
                         .meetSTEP(MeetStep.BEFORE_START)
                         .room(room)
@@ -113,9 +114,10 @@ public class CreateController {
                 tool = Tool.builder()
                         .sixhat(sixhat)
                         .build();
+
                 toolRepository.save(tool);
                 sixhatRepository.save(sixhat);
-                Room room1 = roomRepository.save(room);
+                room = roomRepository.save(room);
 
                 user_has_room = User_has_Room.builder()
                         .user(user)
@@ -124,8 +126,8 @@ public class CreateController {
 
                 user_has_roomRepository.save(user_has_room);
                 RoomDTO roomDTO = RoomDTO.builder()
-                        .roomID(room1.getRoomID())
-                        .title(room1.getTitle())
+                        .roomID(room.getRoomID())
+                        .title(room.getTitle())
                         .hostUSER(UserDTO.builder()
                                 .userNAME(user.getUserNAME())
                                 .build()
@@ -135,7 +137,11 @@ public class CreateController {
                 return new FixedreturnValue<>(roomDTO);
 
             case MIND_MAP:
-                return new FixedreturnValue<>();
+                return FixedreturnValue.builder()
+                        .status(ErrorCode.NOT_FOUND.getStatus())
+                        .message(ErrorCode.NOT_FOUND.getMessage())
+                        .customMessage("아직 마인드맵 안만들었쪙 ㅠㅠ")
+                        .build();
 
         }
 
@@ -147,29 +153,27 @@ public class CreateController {
     }
 
 
-//    @MessageMapping(value = "/save/enterroom")
     @RequestMapping(value = "/create/enterroom" , method = RequestMethod.POST)
     @Transactional
     @LogExecution
     public Object enterRoom(@RequestBody JSONObject jsonObject) throws JsonProcessingException {
         RoomDTO roomDTO;
+        Long id;
         User guestUser = new User();
-
-        log.info(jsonObject.containsKey("id")?
-                jsonObject.get("id").toString():
-                "guest 이용자입니다");
+        int roomID = Integer.parseInt(jsonObject.get("roomID").toString());
 
         if(!jsonObject.containsKey("id")){
+            id = 0L;
+            log.info("guest 이용자입니다");
             guestUser = userRepository.save(
                     User.builder()
                     .userNAME(jsonObject.get("userNAME").toString())
                     .build()
             );
         }else{
-            log.info("이미 참가한 사용자입니다. DB저장없이 리턴만 해줍니당~ ");
-            int roomID = Integer.parseInt(jsonObject.get("roomID").toString());
-            Long id = Long.parseLong(jsonObject.get("id").toString());
+            id = Long.parseLong(jsonObject.get("id").toString());
             if(user_has_roomRepository.findByRoom_RoomIDAndUser_Id(roomID, id).isPresent()){
+                log.info("이미 참가한 사용자입니다. DB저장없이 리턴만 해줍니당~ ");
                 Room room = roomRepository.findByRoomID(roomID).orElseThrow(() ->
                             new NoRoomIDException("룸없음" , ErrorCode.NOT_FOUND)
                         );
@@ -200,11 +204,10 @@ public class CreateController {
                 );
             }
         }
-        Long id = jsonObject.containsKey("id")?Long.parseLong(jsonObject.get("id").toString()):0L;
-        int roomid = Integer.parseInt(jsonObject.get("roomID").toString());
-        Room room  = roomRepository.findByRoomID(roomid).orElseThrow(()->
-                new NotFoundException("해당 방(방 번호 : "+roomid+") 이 없습니다 ", ErrorCode.NOT_FOUND)
+        Room room  = roomRepository.findByRoomID(roomID).orElseThrow(()->
+                new NotFoundException("해당 방(방 번호 : "+roomID+") 이 없습니다 ", ErrorCode.NOT_FOUND)
         );
+
         User user = userRepository.findById(id).orElse(guestUser);
 
 
@@ -238,7 +241,7 @@ public class CreateController {
             );
         }
         roomDTO = RoomDTO.builder()
-                .roomID(roomid)
+                .roomID(roomID)
                 .title(room.getTitle())
                 .hostUSER(
                         UserDTO.builder()
@@ -256,7 +259,7 @@ public class CreateController {
                                 .build()
                 )));
         messagingTemplate.convertAndSend(
-                "/topic/enterroom/"+roomid
+                "/topic/enterroom/"+roomID
                 , new FixedreturnValue<>(
                         UserDTO.builder()
                                 .userNAME(user.getUserNAME())
